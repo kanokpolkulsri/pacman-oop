666aeb6921057d8f0cf204b8981cf5b912350c7d
tonplamm <tonplamm@10.2.31.142>
2016-10-07 14:21:08 +0700

    finish worldren, world

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -9,27 +9,20 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
-	private Texture pacmanImg;
-    private PacmanGame pacmanGame;
-    private Pacman pacman;
+	private Pacman pacman;
     World world;
+    WorldRenderer worldRenderer;
     public GameScreen(PacmanGame pacmanGame) {
-    	this.pacmanGame = pacmanGame;
-    	pacmanImg = new Texture("pacman.png");
+    	new Texture("pacman.png");
     	world = new World(pacmanGame);
     	pacman = world.getPacman();
+    	worldRenderer = new WorldRenderer(pacmanGame, world);
     }
     
     @Override
     public void render(float delta) {
     	update(delta);
-        SpriteBatch batch = pacmanGame.batch;
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        Vector2 pos = pacman.getPosition();
-        batch.draw(pacmanImg, pos.x, pos.y);
-        batch.end();
+        worldRenderer.render(delta);
     }
     
     private void update(float delta) {
---
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class WorldRenderer extends ScreenAdapter {
+	private Texture pacmanImg;
+    private PacmanGame pacmanGame;
+    private Pacman pacman;
+    private SpriteBatch batch;
+    
+	public WorldRenderer(PacmanGame pacmanGame, World world) {
+        this.pacmanGame = pacmanGame;
+        batch = this.pacmanGame.batch;
+        pacmanImg = new Texture("pacman.png");
+        pacman = world.getPacman();
+    }
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Vector2 pos = pacman.getPosition();
+        batch.begin();
+        batch.draw(pacmanImg, pos.x, pos.y);
+        batch.end();
+    }
+}
+
+
